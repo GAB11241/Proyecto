053ac3aade44686d07d72455d2a099860697fff5
gabriel <gabriellopezurzaiz41@gmail.com>
2022-12-16 20:11:04 -0300

    footerListo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { BannerComponent } from './componentes/banner/banner.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BannerComponent,
-    LoginComponent
+    LoginComponent,
+    AcercaDeComponent,
+    ExperienciasComponent,
+    EducacionComponent,
+    SkillsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
